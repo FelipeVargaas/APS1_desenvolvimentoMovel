8e0cbb1b50b81ed6c0abf01b79a30e584d44776c
FelipeVargaas <felipevargasrj@gmail.com>
2023-05-11 21:20:57 -0300

    upando o APP

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APS_NOVO',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
